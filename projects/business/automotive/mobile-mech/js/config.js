8c5976011dffbdb02e3226564b28ba253b1a1cdf
Marlon Santos <marlon@marlsantos.com>
2025-06-03 10:42:00 +1000

    Password protect and PayPal integration on mobile-mech site.

---
+++ projects/business/automotive/mobile-mech/js/config.js
@@ -0,0 +1,15 @@
+// Configuration settings for Mobile Mech pages
+const _0x4f2d=['AUTH','PAYPAL','CLIENT_ID','CURRENCY','SDK_URL','COMPONENTS','AMOUNT','PASSWORD_HASH','COOKIE_NAME','AVIT8mnKPYchpwhbfHD9ti7Sez1Al4470ve4XFhgH-CxREuDU66f9gND_elb7eiFtj5Yqo7uffH6mu_g','AUD','https://www.paypal.com/sdk/js','buttons','10.00','15b92d66e5ec3f2e8fa537e8cc9751cb667623cdcb9bc9afb31beee50b1a1d5c','mobile_mech_paid_access'];
+export const CONFIG={
+    [_0x4f2d[0]]:{
+        [_0x4f2d[7]]:_0x4f2d[14],
+        [_0x4f2d[8]]:_0x4f2d[15]
+    },
+    [_0x4f2d[1]]:{
+        [_0x4f2d[2]]:_0x4f2d[9],
+        [_0x4f2d[3]]:_0x4f2d[10],
+        [_0x4f2d[4]]:_0x4f2d[11],
+        [_0x4f2d[5]]:_0x4f2d[12],
+        [_0x4f2d[6]]:_0x4f2d[13]
+    }
+}; 
